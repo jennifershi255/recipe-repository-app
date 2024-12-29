6d3f9c57cb257659992d365f54583d20067924d5
jennifershi255 <jennifershi255@gmail.com>
2024-12-28 23:21:29 -0500

    Save changes from detached HEAD

--- backend/models.py
+++ backend/models.py
@@ -5,7 +5,7 @@ class Recipe(db.Model):
   name = db.Column(db.String(100), nullable=False)
   description = db.Column(db.Text, nullable=False)
   category = db.Column(db.String(10), nullable=False)
-  img_url = db.Column(db.String(200), nullable=True)
+  link = db.Column(db.String(200), nullable = True)
 
 
   def to_json(self):
@@ -14,7 +14,7 @@ class Recipe(db.Model):
       "name":self.name,
       "description":self.description,
       "category":self.category,
-      "imgUrl":self.img_url,
+      "link":self.link,
     }
 
  
